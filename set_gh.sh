fa34f8e08c6ae9e123cbf101513e3ef20fc538f3
ydzhao <421564807@qq.com>
2024-07-02 16:50:21 +0800

    add gh_proxy shell

---
+++ set_gh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# File              : set_gh.sh
+# Author            : Zhao Ya Dong <421564807@qq.com>
+# Date              : 2024.07.02
+# Last Modified Date: 2024.07.02
+# CopyRight (c) 2021 CPIC DIR, all rights reserved.
+#
+#
+git config --global url."http://ghcf5.jsdelivr.fyi/https://github.com/".insteadOf "https://github.com/"
+git config --global --list
